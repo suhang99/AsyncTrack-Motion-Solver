0f37893abd79f83e8db680922582828274da2027
Ericsii <ericfengx@foxmail.com>
2025-09-03 12:49:19 +0000

    chore: Add LICENSE

--- src/ceres_helper/sphere_manifold.cpp
+++ src/ceres_helper/sphere_manifold.cpp
@@ -1,3 +1,17 @@
+// Copyright 2025 Hang Su, Yunlong Feng, Mobile Perception Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <motion_solver/ceres_helper/sphere_manifold.hpp>
 
 #if CERES_VERSION_MAJOR < 2 || CERES_VERSION_MINOR < 2
